5523498ab512674b62d8303804bfe4c14e9e62a6
Haz3l02 <hgreen2102@gmail.com>
2023-01-29 10:58:00 -0500

    added db fxml and controller and did the basic stuff + buttons of the db fxml (still have to do table)

---
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/DatabaseController.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.C23.teamA.controllers;
+
+import io.github.palexdev.materialfx.controls.MFXTableView;
+import javafx.fxml.FXML;
+
+public class DatabaseController {
+
+    @FXML private MFXTableView<String> dbTable;
+
+
+    /**
+     * runs on switching to this scene
+     */
+    public void initialize() {
+
+
+        // initialize fields in the table
+
+
+
+
+    }
+}
