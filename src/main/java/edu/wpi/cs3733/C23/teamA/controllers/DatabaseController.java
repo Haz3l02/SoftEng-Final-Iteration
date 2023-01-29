ac9e01a3168c6a1f6333383d015f871158414f2b
Haz3l02 <hgreen2102@gmail.com>
2023-01-29 13:35:41 -0500

    table works now, added a class (DBTableRow) to hold data for it

--- src/main/java/edu/wpi/cs3733/C23/teamA/controllers/DatabaseController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/DatabaseController.java
@@ -1,23 +1,34 @@
 package edu.wpi.cs3733.C23.teamA.controllers;
 
-import io.github.palexdev.materialfx.controls.MFXTableView;
+import edu.wpi.cs3733.C23.teamA.DBTableRow;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class DatabaseController {
 
-    @FXML private MFXTableView<String> dbTable;
+  @FXML private TableView<DBTableRow> dbTable;
 
+  @FXML public TableColumn<DBTableRow, String> nodeCol;
+  @FXML public TableColumn<DBTableRow, String> locNameCol;
+  @FXML public TableColumn<DBTableRow, String> moveCol;
+  @FXML public TableColumn<DBTableRow, String> edgeDataCol;
 
-    /**
-     * runs on switching to this scene
-     */
-    public void initialize() {
+  private ObservableList<DBTableRow> dbTableRowsModel =
+      FXCollections.observableArrayList(
+          new DBTableRow("a", "aaaaaaaa", "aaa", "aaaaaaa"),
+          new DBTableRow("bb", "b", "bbb", "bbbbb"));
+  /** runs on switching to this scene */
+  public void initialize() {
 
+    nodeCol.setCellValueFactory(new PropertyValueFactory<>("node"));
+    locNameCol.setCellValueFactory(new PropertyValueFactory<>("locName"));
+    moveCol.setCellValueFactory(new PropertyValueFactory<>("move"));
+    edgeDataCol.setCellValueFactory(new PropertyValueFactory<>("edgeData"));
 
-        // initialize fields in the table
-
-
-
-
-    }
+    dbTable.setItems(dbTableRowsModel);
+  }
 }
