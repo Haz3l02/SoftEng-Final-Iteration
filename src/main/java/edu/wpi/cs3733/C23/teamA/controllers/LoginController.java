0514629d0463cdfe2cbbb9cecaecb7f7c0fec357
smrosario <smrosario@wpi.edu>
2023-02-03 23:23:06 -0500

    Added a login page.

---
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package edu.wpi.cs3733.C23.teamA.controllers;
+
+import edu.wpi.cs3733.C23.teamA.navigation.Navigation;
+import edu.wpi.cs3733.C23.teamA.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class LoginController {
+
+  @FXML MFXTextField usernameTextField;
+  @FXML MFXTextField passwordTextField;
+
+  public void logout(ActionEvent event) {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Logout");
+    alert.setHeaderText("You are about to log out!");
+    alert.setContentText("Unsubmitted information in the form will be lost!");
+
+    if (alert.showAndWait().get() == ButtonType.OK) {
+      System.out.println("You have successfully logged out!");
+      Navigation.close(); // MAY NOT FUCKING WORK
+    }
+  }
+
+  public void login(ActionEvent event) {
+    Navigation.navigate(Screen.HOME);
+  }
+}
