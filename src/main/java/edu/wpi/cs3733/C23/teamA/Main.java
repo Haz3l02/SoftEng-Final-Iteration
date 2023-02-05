7284909471435bdae606426aa3a26b224a6bcd55
Harrison Taylor <htaylor1769@gmail.com>
2023-02-05 15:42:23 -0500

    ADB

--- src/main/java/edu/wpi/cs3733/C23/teamA/Main.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Main.java
@@ -6,26 +6,5 @@ public class Main {
 
     // AApp.launch(AApp.class, args);
 
-    //    Configuration configuration = new Configuration();
-    //    configuration.configure("hibernate.cfg.xml");
-    //    SessionFactory factory = configuration.buildSessionFactory();
-    //    Session session = factory.openSession();
-    //
-    //    Transaction tx = session.beginTransaction();
-    //    //    SanitationrequestEntity sanreq =
-    //    //        new SanitationrequestEntity(
-    //    //            "Clean",
-    //    //            "123",
-    //    //            "there",
-    //    //            "stuff",
-    //    //            ServicerequestEntity.Urgency.HIGH,
-    //    //            ServicerequestEntity.RequestType.SANITATION,
-    //    //            ServicerequestEntity.Status.BLANK,
-    //    //            "John",
-    //    //            SanitationrequestEntity.Category.BIOHAZARD);
-    //    //
-    //    //    session.persist(sanreq);
-    //    tx.commit();
-    //    session.close();
   }
 }
