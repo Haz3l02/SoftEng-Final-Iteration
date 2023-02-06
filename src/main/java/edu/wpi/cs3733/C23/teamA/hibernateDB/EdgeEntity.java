6a3a5096327f2f786b6b32378a81b4b219b50143
Harrison Taylor <htaylor1769@gmail.com>
2023-02-06 11:46:53 -0500

    uniquekey

--- src/main/java/edu/wpi/cs3733/C23/teamA/hibernateDB/EdgeEntity.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/hibernateDB/EdgeEntity.java
@@ -10,22 +10,29 @@ import lombok.Setter;
 public class EdgeEntity {
 
   @Id
+  @Column(name = "edgeid", length = -1)
+  @Getter
+  @Setter
+  private String edgeid;
+
+  // @Id
   @Getter
   @Setter
   @ManyToOne
   @JoinColumn(name = "node1", foreignKey = @ForeignKey(name = "node1"))
   private NodeEntity node1;
 
-  @Id
+  // @Id
   @Getter
   @Setter
   @ManyToOne
   @JoinColumn(name = "node2", foreignKey = @ForeignKey(name = "node2"))
   private NodeEntity node2;
 
-  public EdgeEntity(NodeEntity node1, NodeEntity node2) {
+  public EdgeEntity(NodeEntity node1, NodeEntity node2, String edgeid) {
     this.node1 = node1;
     this.node2 = node2;
+    this.edgeid = edgeid;
   }
 
   public EdgeEntity() {}
@@ -41,6 +48,7 @@ public class EdgeEntity {
     if (this == obj) return true;
     if (this.getClass() != obj.getClass()) return false;
     EdgeEntity ed = (EdgeEntity) obj;
-    return (node1.equals(ed.node1) && node2.equals(ed.node2));
+    return (node1.equals(ed.node1) && node2.equals(ed.node2)
+        || node1.equals(ed.node2) && node2.equals(ed.node1));
   }
 }
