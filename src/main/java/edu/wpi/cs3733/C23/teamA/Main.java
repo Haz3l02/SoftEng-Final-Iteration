655e8898a22202b870c566f5b79b8eda9927d10e
Harrison Taylor <htaylor1769@gmail.com>
2023-02-05 20:54:40 -0500

    LETSGO

--- src/main/java/edu/wpi/cs3733/C23/teamA/Main.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Main.java
@@ -1,7 +1,9 @@
 package edu.wpi.cs3733.C23.teamA;
 
 import edu.wpi.cs3733.C23.teamA.hibernateDB.ADBSingletonClass;
+import edu.wpi.cs3733.C23.teamA.hibernateDB.ComputerrequestEntity;
 import edu.wpi.cs3733.C23.teamA.hibernateDB.EmployeeEntity;
+import edu.wpi.cs3733.C23.teamA.hibernateDB.ServicerequestEntity;
 import jakarta.persistence.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -15,9 +17,22 @@ public class Main {
     Session session = ADBSingletonClass.getSessionFactory().openSession();
     Transaction tx = session.beginTransaction();
 
-    EmployeeEntity person = new EmployeeEntity("123", "staff", "staff", "Medical", "Wilson Wong");
-    // ComputerrequestEntity com = new ComputerrequestEntity("PC help", );
-    session.persist(person);
+    // EmployeeEntity person = new EmployeeEntity("123", "staff", "staff", "Medical", "Wilson
+    // Wong");
+    EmployeeEntity person = session.get(EmployeeEntity.class, "123");
+    ComputerrequestEntity com =
+        new ComputerrequestEntity(
+            "PC help",
+            person,
+            "There",
+            "Need help",
+            ServicerequestEntity.Urgency.EXTREMELY_URGENT,
+            ServicerequestEntity.RequestType.COMPUTER,
+            ServicerequestEntity.Status.BLANK,
+            "Harrison",
+            "365",
+            ComputerrequestEntity.Device.DESKTOP);
+    session.persist(com);
 
     tx.commit();
     session.close();
