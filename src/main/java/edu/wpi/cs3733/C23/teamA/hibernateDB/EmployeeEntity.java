0afc70457417eba18fbbdc65d344280da0a643a2
smrosario <smrosario@wpi.edu>
2023-02-06 23:47:18 -0500

    Trying to fix login.

--- src/main/java/edu/wpi/cs3733/C23/teamA/hibernateDB/EmployeeEntity.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/hibernateDB/EmployeeEntity.java
@@ -73,7 +73,11 @@ public class EmployeeEntity {
     ArrayList<String> info = new ArrayList<String>();
     Transaction tx = session.beginTransaction();
     String hql =
-            "select * from employee emp where emp.username = '" + user+ "' and emp.password = '" + pass + "'";
+        "select * from employee emp where emp.username = '"
+            + user
+            + "' and emp.password = '"
+            + pass
+            + "'";
     Query query = session.createQuery(hql);
     final List<EmployeeEntity> emps = query.getResultList();
     for (EmployeeEntity emp : emps) {
