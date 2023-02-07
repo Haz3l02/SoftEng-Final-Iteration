06c0c783ab83c886fdd7443b862aefd913e88ce8
sarahlarusso <sjlarusso@wpi.edu>
2023-02-07 14:43:31 -0500

    deleted old map display for pathfinding

--- src/main/java/edu/wpi/cs3733/C23/teamA/navigation/Screen.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/navigation/Screen.java
@@ -17,7 +17,6 @@ public enum Screen {
   EDGE("views/EdgeFXML.fxml"),
   PATHFINDING("views/PathfindingFXML.fxml"),
   PATHFINDING_MAP("views/PathfindingMapFXML.fxml"),
-
   SERVICE_REQUEST_STATUS("views/ServiceRequestStatusNewFXML.fxml"),
   ID_INPUT("views/IDInputFXML.fxml"),
   NODE_MAP("views/NodeMapFXML.fxml"),
