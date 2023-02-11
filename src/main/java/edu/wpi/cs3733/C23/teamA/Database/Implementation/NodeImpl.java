a289d96bfc6425f5cfa61836ac133b825bf012f7
Harrison Taylor <htaylor1769@gmail.com>
2023-02-11 03:30:45 -0500

    stuff

--- src/main/java/edu/wpi/cs3733/C23/teamA/Database/Implementation/NodeImpl.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Database/Implementation/NodeImpl.java
@@ -1,3 +1,35 @@
 package edu.wpi.cs3733.C23.teamA.Database.Implementation;
 
-public class NodeImpl {}
+import edu.wpi.cs3733.C23.teamA.Database.API.IDatabaseAPI;
+import edu.wpi.cs3733.C23.teamA.Database.Entities.NodeEntity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class NodeImpl implements IDatabaseAPI<NodeEntity, String> {
+
+    public List<NodeEntity> getAll() {
+        return null;
+    }
+
+    public void add(NodeEntity obj) {
+
+    }
+
+    public void importFromCSV(String filename) throws FileNotFoundException {
+
+    }
+
+    public void exportToCSV(String filename) throws IOException {
+
+    }
+
+    public void update(String ID, NodeEntity obj) {
+
+    }
+
+    public void delete(NodeEntity obj) {
+
+    }
+}
