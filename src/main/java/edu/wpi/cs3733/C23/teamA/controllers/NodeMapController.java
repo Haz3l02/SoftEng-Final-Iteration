80c54b90664c0f50d2cf7ecc57e80f3657805df6
chriswalczak2 <cwalczak2@wpi.edu>
2023-02-11 16:23:39 -0500

    circles are selectable!!!

--- src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NodeMapController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NodeMapController.java
@@ -16,9 +16,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import net.kurobako.gesturefx.GesturePane;
 import org.hibernate.Session;
@@ -51,37 +49,37 @@ public class NodeMapController extends ServiceRequestController {
 
     // Add nodes as circles
     // gc = nodeMapCanvas.getGraphicsContext2D();
-    NodeDraw.drawNodes(allNodes, 0.16, nodeAnchor);
+    NodeDraw.drawNodes(allNodes, 0.15, nodeAnchor);
 
     // Pane
 
-    nodeAnchor.setOnMouseClicked(
-        e -> {
-          double x = e.getX();
-          double y = e.getY();
-          System.out.println(x);
-          System.out.println(" " + y);
-          if (e.getButton() == MouseButton.PRIMARY) {
+    //        nodeAnchor.setOnMouseClicked(
+    //            e -> {
+    //              double x = e.getX();
+    //              double y = e.getY();
+    //              System.out.println(x);
+    //              System.out.println(" " + y);
+    //              if (e.getButton() == MouseButton.PRIMARY) {
+    //
+    //                final Pane nodeGraphic = new Pane();
+    //
+    //                /* Set the style of the node */
+    //                nodeGraphic.setPrefSize(4, 4);
+    //                nodeGraphic.setLayoutX(x - 4);
+    //                nodeGraphic.setLayoutY(y - 4);
+    //                nodeGraphic.setStyle(
+    //                    "-fx-background-color: '#F6BD38'; "
+    //                        + "-fx-background-radius: 12.5; "
+    //                        + "-fx-border-color: '013A75'; "
+    //                        + "-fx-border-width: 1;"
+    //                        + "-fx-border-radius: 12.5");
+    //
+    //                nodeAnchor.getChildren().add(nodeGraphic);
+    //
+    //              } else if (e.getButton() == MouseButton.SECONDARY) {
+    //
+    //              }
 
-            final Pane nodeGraphic = new Pane();
-
-            /* Set the style of the node */
-            nodeGraphic.setPrefSize(4, 4);
-            nodeGraphic.setLayoutX(x - 4);
-            nodeGraphic.setLayoutY(y - 4);
-            nodeGraphic.setStyle(
-                "-fx-background-color: '#F6BD38'; "
-                    + "-fx-background-radius: 12.5; "
-                    + "-fx-border-color: '013A75'; "
-                    + "-fx-border-width: 1;"
-                    + "-fx-border-radius: 12.5");
-
-            nodeAnchor.getChildren().add(nodeGraphic);
-
-          } else if (e.getButton() == MouseButton.SECONDARY) {
-
-          }
-        });
   }
 
   /**
--- src/main/java/edu/wpi/cs3733/C23/teamA/mapeditor/NodeDraw.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/mapeditor/NodeDraw.java
@@ -3,11 +3,14 @@ package edu.wpi.cs3733.C23.teamA.mapeditor;
 import edu.wpi.cs3733.C23.teamA.hibernateDB.NodeEntity;
 import java.util.List;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class NodeDraw {
+
+  static Node previousNode = null;
 
   private static int[] scaleCoordinates(double xCoord, double yCoord, double scaleFactor) {
     // get the coordinates from the node
@@ -30,16 +33,12 @@ public class NodeDraw {
     // draw circle for each node
     for (NodeEntity n : allNodes) {
       int[] updatedCoords = scaleCoordinates(n.getXcoord(), n.getYcoord(), scaleFactor);
-      // gc.fillOval(updatedCoords[0] - 4, updatedCoords[0] - 4, 8, 8);
-      // Circle nodePoint = new Circle(updatedCoords[0], updatedCoords[1], 4,
-      // Color.web("0x224870"));
-      // System.out.pr
-      final Pane nodeGraphic = new Pane();
+      Pane nodeGraphic = new Pane();
 
       /* Set the style of the node */
       nodeGraphic.setPrefSize(4, 4);
-      nodeGraphic.setLayoutX(updatedCoords[0] - 4);
-      nodeGraphic.setLayoutY(updatedCoords[1] - 4);
+      nodeGraphic.setLayoutX(updatedCoords[0] - 2);
+      nodeGraphic.setLayoutY(updatedCoords[1] - 2);
       nodeGraphic.setStyle(
           "-fx-background-color: '#F6BD38'; "
               + "-fx-background-radius: 12.5; "
@@ -49,17 +48,36 @@ public class NodeDraw {
 
       EventHandler<MouseEvent> eventHandler =
           new EventHandler<MouseEvent>() {
+
             @Override
-            public void handle(MouseEvent e) {
+            public void handle(MouseEvent event) {
               System.out.println("Hello World");
+
+              if ((previousNode != null)) {
+                System.out.println("made it");
+                if (!previousNode.equals(nodeGraphic)) {
+                  System.out.println("setting yellow");
+                  previousNode.setStyle(
+                      "-fx-background-color: '#F6BD38'; "
+                          + "-fx-background-radius: 12.5; "
+                          + "-fx-border-color: '013A75'; "
+                          + "-fx-border-width: 1;"
+                          + "-fx-border-radius: 12.5");
+                }
+              }
+
               nodeGraphic.setStyle(
                   "-fx-background-color: 'red'; "
                       + "-fx-background-radius: 12.5; "
-                      + "-fx-border-color: '013A75'; "
+                      + "-fx-border-color: 'red'; "
                       + "-fx-border-width: 1;"
                       + "-fx-border-radius: 12.5");
+
+              previousNode = nodeGraphic;
             }
           };
+      nodeGraphic.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+      // nodeGraphic.setOnMouseClicked(event -> System.out.println("click"));
 
       nodeAnchor.getChildren().add(nodeGraphic);
     }
