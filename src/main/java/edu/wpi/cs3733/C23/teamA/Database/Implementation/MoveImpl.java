e3828fa52b134256613a61504c870c23ce874d64
Harrison Taylor <htaylor1769@gmail.com>
2023-02-14 17:40:22 -0500

    movesUpdate

--- src/main/java/edu/wpi/cs3733/C23/teamA/Database/Implementation/MoveImpl.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Database/Implementation/MoveImpl.java
@@ -272,7 +272,7 @@ public class MoveImpl implements IDatabaseAPI<MoveEntity, List<String>> {
                             "mov.node = :newnodeid, mov.locationName = :newlongname, mov.movedate = :newmovedate " +
                             "WHERE mov.node = :oldnodeid and mov.locationName = :oldlongname and mov.movedate = :oldmovedate")
             .setParameter("newnodeid", session.get(NodeEntity.class, obj.getNode().getNodeid()))
-            .setParameter("newlongname", session.get(NodeEntity.class, obj.getNode().getNodeid()))
+            .setParameter("newlongname", session.get(LocationNameEntity.class, obj.getLocationName().getLongname()))
             .setParameter("newmovedate", obj.getMovedate())
             .setParameter("oldnodeid", session.get(NodeEntity.class, ID.get(0)))
             .setParameter("oldlongname", session.get(LocationNameEntity.class, ID.get(1)))
