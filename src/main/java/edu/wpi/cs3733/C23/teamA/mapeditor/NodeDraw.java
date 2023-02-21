f71e28e8ad92e1f7faa138470c78a1f49c44a99e
chriswalczak2 <cwalczak2@wpi.edu>
2023-02-20 20:40:31 -0500

    node drag in progress

--- src/main/java/edu/wpi/cs3733/C23/teamA/mapeditor/NodeDraw.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/mapeditor/NodeDraw.java
@@ -7,7 +7,6 @@ import edu.wpi.cs3733.C23.teamA.controllers.MapEditorController;
 import edu.wpi.cs3733.C23.teamA.pathfinding.enums.Floor;
 import java.util.List;
 import javafx.event.EventHandler;
-import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -20,19 +19,15 @@ import javafx.scene.text.Text;
 
 public class NodeDraw {
 
-  static Pane previousNode = null;
+  static Pane previousSelectedNode = null;
   static Pane selectNodePane = null;
-  static NodeEntity selectedNode = null;
+  static NodeEntity selectedNodeEntity = null;
+
+  static Pane currentPane = new Pane();
 
   static Line selectedLine = null;
   static Line previousLine = null;
   static EdgeEntity selectedEdge = null;
-
-  static MapEditorController MEC = new MapEditorController();
-
-  static MakeDraggable draggable = new MakeDraggable();
-
-  // static MoveImpl moveImp = new MoveImpl();
 
   static boolean setLocationVisibility;
 
@@ -41,7 +36,7 @@ public class NodeDraw {
   public static void setVisibility(boolean b) {}
 
   public static NodeEntity getSelected() {
-    return selectedNode;
+    return selectedNodeEntity;
   }
 
   public static Pane getSelectedPane() {
@@ -49,7 +44,7 @@ public class NodeDraw {
   }
 
   public static void setSelectedPane(Pane p) {
-    previousNode = p;
+    previousSelectedNode = p;
   }
 
   public static int[] scaleCoordinates(double xCoord, double yCoord, double scaleFactor) {
@@ -102,6 +97,8 @@ public class NodeDraw {
       int[] updatedCoords = scaleCoordinates(n.getXcoord(), n.getYcoord(), scaleFactor);
       Pane nodeGraphic = new Pane();
 
+      currentPane = nodeGraphic;
+
       /* Set the style of the node */
       nodeGraphic.setPrefSize(5, 5);
       nodeGraphic.setLayoutX(updatedCoords[0] - 2.5);
@@ -118,15 +115,15 @@ public class NodeDraw {
           event -> {
             selectNodePane = nodeGraphic;
 
-            if ((previousNode != null)) {
-              if (!previousNode.equals(nodeGraphic)) {
-                previousNode.setStyle(
+            if ((previousSelectedNode != null)) {
+              if (!previousSelectedNode.equals(nodeGraphic)) {
+                previousSelectedNode.setStyle(
                     "-fx-background-color: '#224870'; "
                         + "-fx-background-radius: 12.5; "
                         + "-fx-border-color: '#224870'; "
                         + "-fx-border-width: 1;"
                         + "-fx-border-radius: 13.5");
-                previousNode.setPrefSize(5, 5);
+                previousSelectedNode.setPrefSize(5, 5);
                 selectedLine.setStroke(Color.web("0x224870"));
                 previousLine.setStrokeWidth(1);
               }
@@ -139,11 +136,11 @@ public class NodeDraw {
                     + "-fx-border-width: 1;"
                     + "-fx-border-radius: 13.5");
             nodeGraphic.setPrefSize(7, 7);
-            //                            nodeGraphic.setLayoutX(nodeGraphic.getXcoord() - 3.5);
-            //                            nodeGraphic.setLayoutY(nodeGraphic.getYcoord() - 3.5);
+            //                            nodeGraphic.setLayoutX(nodeGraphic.getXcoord() -3.5);
+            //                            nodeGraphic.setLayoutY(nodeGraphic.getYcoord() -3.5);
 
-            previousNode = nodeGraphic;
-            selectedNode = n;
+            previousSelectedNode = nodeGraphic;
+            selectedNodeEntity = n;
             nmc.setXCord(n.getXcoord().toString());
             nmc.setYCord(n.getYcoord().toString());
             nmc.setFloorBox(Floor.extendedStringFromTableString(n.getFloor()));
@@ -165,69 +162,65 @@ public class NodeDraw {
       nodeGraphic.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
 
       // for hover over node
-      EventHandler<MouseEvent> eventHandler2 =
-          new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-              if ((!nodeGraphic.equals(selectNodePane))) {
-                nodeGraphic.setStyle(
-                    "-fx-background-color: 'green'; "
-                        + "-fx-background-radius: 12.5; "
-                        + "-fx-border-color: 'green'; "
-                        + "-fx-border-width: 1;"
-                        + "-fx-border-radius: 13.5");
-              }
-            }
-          };
-      nodeGraphic.addEventFilter(MouseEvent.MOUSE_ENTERED, eventHandler2);
-
-      EventHandler<MouseEvent> eventHandler3 =
-          new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-              if ((!nodeGraphic.equals(selectNodePane))) {
-                nodeGraphic.setStyle(
-                    "-fx-background-color: '#224870'; "
-                        + "-fx-background-radius: 12.5; "
-                        + "-fx-border-color: '#224870'; "
-                        + "-fx-border-width: 1;"
-                        + "-fx-border-radius: 13.5");
-              }
-            }
-          };
-      nodeGraphic.addEventFilter(MouseEvent.MOUSE_EXITED, eventHandler3);
-
-      nodeGraphic.setOnMouseClicked(
-          new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-              if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
-                if (mouseEvent.getClickCount() == 2) {
-                  System.out.println("Double clicked");
-                  // draggable.makeDraggable(nodeGraphic);
-
-                }
-              }
-            }
-          });
+      //            EventHandler<MouseEvent> eventHandler2 =
+      //                new EventHandler<MouseEvent>() {
+      //                  @Override
+      //                  public void handle(MouseEvent event) {
+      //                    if ((!nodeGraphic.equals(selectNodePane))) {
+      //                      nodeGraphic.setStyle(
+      //                          "-fx-background-color: 'green'; "
+      //                              + "-fx-background-radius: 12.5; "
+      //                              + "-fx-border-color: 'green'; "
+      //                              + "-fx-border-width: 1;"
+      //                              + "-fx-border-radius: 13.5");
+      //                    }
+      //                  }
+      //                };
+      //            nodeGraphic.addEventFilter(MouseEvent.MOUSE_ENTERED, eventHandler2);
+      //
+      //      EventHandler<MouseEvent> eventHandler3 =
+      //          new EventHandler<MouseEvent>() {
+      //            @Override
+      //            public void handle(MouseEvent event) {
+      //              if ((!nodeGraphic.equals(selectNodePane))) {
+      //                nodeGraphic.setStyle(
+      //                    "-fx-background-color: '#224870'; "
+      //                        + "-fx-background-radius: 12.5; "
+      //                        + "-fx-border-color: '#224870'; "
+      //                        + "-fx-border-width: 1;"
+      //                        + "-fx-border-radius: 13.5");
+      //              }
+      //            }
+      //          };
+      //      nodeGraphic.addEventFilter(MouseEvent.MOUSE_EXITED, eventHandler3);
 
       nodeGraphic.setOnMouseDragged(
           new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-              nmc.getMainGesturePane().setGestureEnabled(false);
-              selectNodePane.setLayoutX(nodeGraphic.getLayoutX() + mouseEvent.getX());
-              selectNodePane.setLayoutY(nodeGraphic.getLayoutY() + mouseEvent.getY());
+              if (selectNodePane != null) {
+                nmc.getMainGesturePane().setGestureEnabled(false);
+                selectNodePane.setLayoutX(selectNodePane.getLayoutX() + mouseEvent.getX());
+                selectNodePane.setLayoutY(selectNodePane.getLayoutY() + mouseEvent.getY());
+              }
             }
           });
 
-      nodeGraphic.setOnMouseDragReleased(
+      nodeGraphic.setOnMouseReleased(
           new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent event) {
-              System.out.println("node dropped");
-              nmc.getMainGesturePane().setGestureEnabled(true);
+              if (!event.isStillSincePress()) {
+                System.out.println("node dropped");
+                nmc.getMainGesturePane().setGestureEnabled(true);
+                System.out.println((int) selectNodePane.getLayoutX());
+                System.out.println((int) selectNodePane.getLayoutY());
+                n.setXcoord((int) selectNodePane.getLayoutX());
+                n.setYcoord((int) selectNodePane.getLayoutY());
+
+                drawNodes(allNodes, scaleFactor, nodeAnchor, nmc);
+              }
             }
           });
 
@@ -264,13 +257,13 @@ public class NodeDraw {
                 previousLine.setStroke(Color.web("0x224870"));
                 previousLine.setStrokeWidth(1);
 
-                previousNode.setStyle(
+                previousSelectedNode.setStyle(
                     "-fx-background-color: '#224870'; "
                         + "-fx-background-radius: 12.5; "
                         + "-fx-border-color: '#224870'; "
                         + "-fx-border-width: 1;"
                         + "-fx-border-radius: 13.5");
-                previousNode.setPrefSize(5, 5);
+                previousSelectedNode.setPrefSize(5, 5);
               }
             }
 
