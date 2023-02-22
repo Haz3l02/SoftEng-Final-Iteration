b2f3478af14764fd4f969e2b2adf7b2495c56b6d
Harrison Taylor <htaylor1769@gmail.com>
2023-02-21 19:41:54 -0500

    stuff

--- src/main/java/edu/wpi/cs3733/C23/teamA/Database/Entities/Implementation/ServiceRequestImpl.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Database/Entities/Implementation/ServiceRequestImpl.java
@@ -283,13 +283,13 @@ public class ServiceRequestImpl extends Observable
     ArrayList<ServiceRequestEntity> fin = new ArrayList<>();
 
     for (ServiceRequestEntity ser : services) {
-      if (Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 1
+      if (Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 1
               && ser.getUrgency() == UrgencyLevel.EXTREMELY
-          || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 3
+          || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 3
               && ser.getUrgency() == UrgencyLevel.HIGH
-          || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 5
+          || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 5
               && ser.getUrgency() == UrgencyLevel.MEDIUM
-          || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 8
+          || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 8
               && ser.getUrgency() == UrgencyLevel.LOW) {
         fin.add(ser);
       }
@@ -303,16 +303,16 @@ public class ServiceRequestImpl extends Observable
 
     for (ServiceRequestEntity ser : services) {
       if (ser.getEmployeeAssigned()!=null){
-        if (Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 1
+        if (Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 1
                 && ser.getUrgency() == UrgencyLevel.EXTREMELY
                 && ser.getEmployeeAssigned().getUsername().equals(user)
-                || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 3
+                || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 3
                 && ser.getUrgency() == UrgencyLevel.HIGH
                 && ser.getEmployeeAssigned().getUsername().equals(user)
-                || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 5
+                || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 5
                 && ser.getUrgency() == UrgencyLevel.MEDIUM
                 && ser.getEmployeeAssigned().getUsername().equals(user)
-                || Timestamp.from(Instant.now()).getDay() - ser.getDate().getDay() < 8
+                || Timestamp.from(Instant.now()).toLocalDateTime().getDayOfYear() - ser.getDate().toLocalDateTime().getDayOfYear() > 8
                 && ser.getUrgency() == UrgencyLevel.LOW
                 && ser.getEmployeeAssigned().getUsername().equals(user)) {
           fin.add(ser);
