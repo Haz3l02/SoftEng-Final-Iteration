11b161bd6eaea142a3022a21b0fda4a307b67326
Jay <jcmezzo@wpi.edu>
2023-02-24 15:19:49 -0500

    more chart set up

--- src/main/java/edu/wpi/cs3733/C23/teamA/controllers/SRChartController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/SRChartController.java
@@ -1,4 +1,19 @@
 package edu.wpi.cs3733.C23.teamA.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class SRChartController {
+
+    @FXML
+    private BarChart srChart;
+
+    @FXML
+    public void initialize() throws SQLException, IOException {
+
+    }
 }
