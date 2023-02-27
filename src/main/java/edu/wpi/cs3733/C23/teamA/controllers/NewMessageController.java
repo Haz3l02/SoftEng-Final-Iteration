c5a6ade332d9f86bd68f4710a5740eccb9a880a8
Haz3l02 <hgreen2102@gmail.com>
2023-02-27 12:57:13 -0500

    fixed errors

--- src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NewMessageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NewMessageController.java
@@ -1,7 +1,16 @@
 package edu.wpi.cs3733.C23.teamA.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class NewMessageController {
 
 
+    @FXML
+    void hidePopup(ActionEvent event) {
+    }
 
+    @FXML
+    void sendMessage(ActionEvent event) {
+    }
 }
