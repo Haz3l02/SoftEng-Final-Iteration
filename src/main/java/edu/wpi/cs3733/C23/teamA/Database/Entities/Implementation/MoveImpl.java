5aa58ce34ddd974f4fc6c71f56216c4c8f5004f6
Harrison Taylor <htaylor1769@gmail.com>
2023-02-27 19:18:24 -0500

    NEWMETHODYIPEE

--- src/main/java/edu/wpi/cs3733/C23/teamA/Database/Entities/Implementation/MoveImpl.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/Database/Entities/Implementation/MoveImpl.java
@@ -310,7 +310,7 @@ public class MoveImpl extends Observable implements IDatabaseAPI<MoveEntity, Lis
     return mov;
   }
 
-  public MoveEntity nodeOnOrBeforeDate(String id, LocalDate date) {
+  public MoveEntity moveOnOrBeforeDate(String id, LocalDate date) {
     MoveEntity mov = new MoveEntity();
     List<MoveEntity> ids =
         moves.stream()
@@ -445,16 +445,14 @@ public class MoveImpl extends Observable implements IDatabaseAPI<MoveEntity, Lis
     }
   }
 
-  public ArrayList<NodeEntity> newAndOldNode(String longName, LocalDate date) {
-    ArrayList<NodeEntity> fin = new ArrayList<>();
-    for (MoveEntity m : moves) {
-      if (m.getLocationName().getLongname().equals(longName) && m.getMovedate().equals(date)) {
-        fin.add(m.getNode());
-      }
-    }
 
-    date.minusDays(1);
-    fin.add(nodeOnOrBeforeDate(longName, date).getNode());
+
+  public ArrayList<MoveEntity> newAndOldMove(String longName, LocalDate date) {
+    ArrayList<MoveEntity> fin = new ArrayList<>();
+    fin.add(moveOnOrBeforeDate(longName, date));
+
+    date = fin.get(0).getMovedate().minusDays(1);
+    fin.add(moveOnOrBeforeDate(longName, date));
     return fin;
   }
 }
