ce617aa72fdc90a533f4f9d2b39252b739463bc4
smrosario <smrosario@wpi.edu>
2023-02-28 20:05:16 -0500

    Kiosk is better now

--- src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NewMessageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamA/controllers/NewMessageController.java
@@ -11,7 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 
 public class NewMessageController {
 
